cff472cc0255fab81d92a54c64302376892b0eee
Luis Arenas <luisrubin55@gmail.com>
2025-07-02 17:14:32 -0600

    Nuevos ejercicios de Hackrank

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(metodo(5));
+       operacionesLoop();
     }
-    public static int metodo(int x){
-        return x > 5 ? metodo(x-1):5;
+
+    public void formatearTablaCodigo(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf("%-15s %03d%n", s1, x);
+        }
+        System.out.println("================================");
+    }
+
+    public static void loopTablaMultiplicar() throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+        bufferedReader.close();
+    }
+
+    public static void operacionesLoop(){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+        in.close();
     }
 }
